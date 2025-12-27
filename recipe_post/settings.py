c64ab8bc29e02e22112d892f381c3e6432fe7efe
fullstack0123 <huobing0123@gmail.com>
2025-12-27 18:57:55 +0900

    ALLOWED_HOSTSの再設定

--- recipe_post/settings.py
+++ recipe_post/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zj*e6b@vmu$dh=m5jyb(u8rn9$4kb4)f3kczy1qlhms&psu+vc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['recipe-post-app-7u6m.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
